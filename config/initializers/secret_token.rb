1f4d828e9f937ca6469edb82f4b1c5fc845dba1b
Abhi Mediratta <abhimediratta@gmail.com>
2014-04-23 23:35:53 +0530

    initial commit with users and session

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotoSharing::Application.config.secret_token = '98ba017f099c3cbcb50f864ffac461ce665101f5fb9b326248c9d7558a558bdcd59fa0e9d2e075126e46275bf890567bb35e0edc801b05114c658db604ff4bf2'
